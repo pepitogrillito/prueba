32650067633a840a63beb209da630f711b756965
pepitogrillito <mauromojica32@gmail.com>
2021-04-15 11:50:06 +0200

    Nueva versión
    Nuevos mensajes
    
    Signed-off-by: Pepitogrillito <mauromojica32@gmail.com>

--- Github/src/prueba.java
+++ Github/src/prueba.java
@@ -3,6 +3,8 @@ public class prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Como estás?");
+		System.out.println("Que tal?");
 
 	}
 
